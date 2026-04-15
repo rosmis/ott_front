119ba7262fd352f3eaeb7367923870fce68c3e68
rosmis <rosmis123@gmail.com>
2026-04-15 13:51:43 +0200

    refactor strip boilerplate

---
+++ app/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const { isAuthenticated, logout } = useSanctumAuth()
+</script>
+
+<template>
+  <UHeader>
+    <template #left>
+      <NuxtLink to="/">
+        <AppLogo class="w-auto h-6 shrink-0" />
+      </NuxtLink>
+    </template>
+
+    <template #right>
+      <template v-if="isAuthenticated">
+        <UButton
+          label="Logout"
+          icon="i-lucide-log-out"
+          color="neutral"
+          variant="ghost"
+          @click="logout()"
+        />
+      </template>
+    </template>
+  </UHeader>
+</template>
